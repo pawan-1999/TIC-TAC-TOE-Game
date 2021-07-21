a27d01982ea5352bac7263c0e1d5841d12ce263c
Pawan gupta <75203671+pawan-1999@users.noreply.github.com>
2021-07-21 11:47:29 +0530

    Pawan - Added : Main Method of Tic Tac Toe Game

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Tic_Tac_Toe_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TicTacToeGame obj = new TicTacToeGame();
+            obj.gameBoard();
+        }
+    }
+}
