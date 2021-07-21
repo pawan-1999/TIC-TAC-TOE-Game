6edb3bf16d36a746cc1e9755ac012f363d2cf36b
Pawan gupta <75203671+pawan-1999@users.noreply.github.com>
2021-07-21 12:37:43 +0530

    Pawan - Added : Player input

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -6,6 +6,9 @@ namespace Tic_Tac_Toe_Game
 {
     class TicTacToeGame
     {
+        public const char X = 'X';
+        public const char O = 'O';
+
         public void gameBoard()
         {
             char[] gameBoard = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' , ' '};
@@ -14,6 +17,22 @@ namespace Tic_Tac_Toe_Game
             Console.WriteLine(gameBoard[5] + " " + gameBoard[6] + " " + gameBoard[7]);
             Console.WriteLine("-------------");
             Console.WriteLine(gameBoard[8] + " " + gameBoard[9] + " " + gameBoard[10]);
+        }
+
+        public void abillityToInput()
+        {
+            Random random = new Random();
+            int letterCheck = random.Next(0, 2);
+            switch (letterCheck)
+            {
+                case 0:
+                    Console.WriteLine("You have assigned " +O) ;
+                    break;
+                case 1:
+                    Console.WriteLine("You have assigned " +X);
+                    break;
+            }
+
         }
     }
 }
