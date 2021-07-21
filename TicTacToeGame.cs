2ba4a7f833502d15f275d7ccbab6278d25a70a49
Pawan gupta <75203671+pawan-1999@users.noreply.github.com>
2021-07-21 20:59:29 +0530

    Pawan - Added : Blank Game Board To Play Tic Tac Toe

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -6,14 +6,13 @@ namespace Tic_Tac_Toe_Game
 {
     class TicTacToeGame
     {
-        public void gameBoard()
-        {
-            char[] gameBoard = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' , ' '};
-            Console.WriteLine(gameBoard[2]+ " " + gameBoard[3] + " " + gameBoard[4]);
-            Console.WriteLine("-------------");
-            Console.WriteLine(gameBoard[5] + " " + gameBoard[6] + " " + gameBoard[7]);
-            Console.WriteLine("-------------");
-            Console.WriteLine(gameBoard[8] + " " + gameBoard[9] + " " + gameBoard[10]);
+        char[] Board = new char[10];
+        public  void gameBoard()
+        {  
+            for (int i = 1 ; i < 9 ; i++)
+            {
+                Board[i] = ' '; 
+            }
         }
     }
 }
